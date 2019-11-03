72891489b8629e100e38c2ccbb0d2ac96961a5b2
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-03 19:05:38 +0100

    Player jumping, gravity changed, player movement updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,46 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Configuration parameters
     [SerializeField] float runningSpeed = 5f;
+    [SerializeField] float jumpingSpeed = 5f;
+
+    // Cached components
     Rigidbody2D playerRigidBody;
+    Animator playerAnimator;
+
+    // States
+    private bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidBody = GetComponent<Rigidbody2D>();
+        InitialAssignments();
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerRun();
+        PlayerJump();
         FlipPlayerSprite();
+    }
+
+    // Assigning components
+    private void InitialAssignments()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Jump the player
+    private void PlayerJump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpingSpeed);
+            playerRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     // Run the player
@@ -31,16 +57,21 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runningSpeed, playerRigidBody.velocity.y);
         // Set the palyer velocity
         playerRigidBody.velocity = playerVelocity;
+
+        bool playerIsMoving = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        // Execute running animation 
+        playerAnimator.SetBool("Running", playerIsMoving);
     }
 
     // Flip sprite according to the direction of player
     private void FlipPlayerSprite()
     {
         // Check if player is moving 
-        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
+        bool playerIsMoving = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerIsMoving)
         {
-            // Change the sign of player X Scale according to the direction
+            // Mathf.Sign returns 1 if value is positive or 0, and -1 if it is negative
+            // Moving to the right would give positive velocity(To the left is negative)
             transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1f);
         }
     }
