5f86096c5bdd40921bf50c25d939738f6a2f675e
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-05 19:32:58 +0100

    Double incrementing score bug fixed for coins, saving current coin config added

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -6,14 +6,20 @@ public class CoinPickUp : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickUpSound;
     [SerializeField] int pointsForCoin = 100;
+    private bool coinPicked = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Increment score
-        FindObjectOfType<GameSession>().AddToScore(pointsForCoin);
-        // Execute coin picking up sound effect
-        AudioSource.PlayClipAtPoint(coinPickUpSound, Camera.main.transform.position);
-        // When collided with player, destroy itself
-        Destroy(gameObject);
+        // Check if coin was picked (To avoid double counting score)
+        if (!coinPicked)
+        {
+            coinPicked = true;
+            // Increment score
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoin);
+            // Execute coin picking up sound effect
+            AudioSource.PlayClipAtPoint(coinPickUpSound, Camera.main.transform.position);
+            // When collided with player, destroy itself
+            Destroy(gameObject);
+        }
     }
 }
