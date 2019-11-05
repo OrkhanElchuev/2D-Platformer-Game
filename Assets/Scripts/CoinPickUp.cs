55e727aab5b57351aced62713d1427d1ae40e202
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-05 18:18:09 +0100

    Coin picking up added

---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // When collided with player, destroy itself
+        Destroy(gameObject);
+    }
+}
