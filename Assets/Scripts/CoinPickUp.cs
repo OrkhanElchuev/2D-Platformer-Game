4a8317bbdbb6f8e443ec5b07da107975e9df2799
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-05 18:33:12 +0100

    Pick up sound added

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSound;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Execute coin picking up sound effect
+        AudioSource.PlayClipAtPoint(coinPickUpSound, Camera.main.transform.position);
         // When collided with player, destroy itself
         Destroy(gameObject);
     }
