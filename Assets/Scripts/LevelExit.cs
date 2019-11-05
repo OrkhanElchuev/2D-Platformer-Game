ad7230ddf37a34ae26c834d5a7600e6f7af2cafc
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-05 02:54:21 +0100

    Transition between levels created, Start Menu bg created

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 2f;
+
+    // When player collides with Level Exit sign execute LoadNextLevel
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    // Load Next Level after defined delay (2f)
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        // Get the current scene index and assign it
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Load next scene
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
